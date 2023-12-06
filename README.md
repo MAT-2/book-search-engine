00952525d1db06ca0c83904ed39c352b9c2514db
MAT-2 <mpcasey30@gmail.com>
2023-12-05 21:21:29 -0600

    Added references in README. Added middleware and imports for Apollo GraphQL to work within the application.

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ Refer to LICENSE
 - https://www.apollographql.com/docs/apollo-server/schema/schema
 - https://www.npmjs.com/package/dotenv
 - https://www.apollographql.com/docs/react/data/mutations/
+- https://www.apollographql.com/docs/react/migrating/apollo-client-3-migration/#apollo-link-and-apollo-link-http
+- https://www.apollographql.com/docs/react/api/link/apollo-link-http/
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,48 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+// Imported Apollo GraphQL into App.JSX
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
+//This sets our link for GraphQL
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+//This is middleware to implement a token for every request.
+//There's also a retrieval for a token if it exists within local storage.
+//Headers are returned are returned if a token is present.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+//Middleware for our GraphQL when we are using the const authLink variable.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+//Added ApolloProvider to render Apollo GraphQL into the application.
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
